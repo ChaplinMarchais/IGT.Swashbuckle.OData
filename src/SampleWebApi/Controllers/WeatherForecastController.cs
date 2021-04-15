04f122917598512c8cddcbebb888d79c26444a45
Chaplin Marchais <35165054+ChaplinMarchais@users.noreply.github.com>
2021-04-15 02:35:14 +0000

    Added a solution file to the src folder

---
+++ src/SampleWebApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+using Microsoft.Extensions.Logging;
+
+namespace IGT.Swashbuckle.OData.SampleWebApi.Controllers
+{
+    // [ApiController]
+    [Route("[controller]")]
+    [ApiExplorerSettings(IgnoreApi = false)]
+    public class WeatherForecastController : ODataController
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet, EnableQuery]
+        public IActionResult Get(WeatherForecast? weather = null)
+        {
+            var rng = new Random();
+            return weather is null ? Ok(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })) : Ok(weather);
+        }
+    }
+}
